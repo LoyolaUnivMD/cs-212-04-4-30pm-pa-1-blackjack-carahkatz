dff91a71585ebdbabe74eb2abd9a55d23d289e74
carahkatz <carah.katz@gmail.com>
2024-02-27 21:52:49 -0500

    Design updated. Testcases updated.

--- Deck.java
+++ Deck.java
@@ -4,21 +4,29 @@ import java.util.Random;
 public class Deck {
     Scanner input = new Scanner(System.in);
 
-    // Initialize deck array
-    String [] deck = new String[]{"AS", "1S", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AD", "1D", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AC", "1C", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AH", "1H", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH"};
-
-    // Initialize values from call instance
+    // Initialize values
     int count;
     String [] cards;
-    int cardValue;
 
-    public Deck(int count, String [] cards){
-        this.count = count;
+    public Deck(String [] cards, int count){
         this.cards = cards;
+        this.count = count;
     }
 
+    // Display method shows current hands
+    public static void display(String[] dealerHand, String[] playerHand) {
+
+        System.out.println("Dealer: " + dealerHand[0] + " " + dealerHand[1]);
+        System.out.println("Player: " + playerHand[0] + " " + playerHand[1]);
+        System.out.println("Hit(1) or Stay(2)");
+    }
+
+
     // Draw method returns random value from this array
-    public String[] hit() {
+    public static String[] hit(String[] cards, int count) {
+        // Initialize deck array
+        String [] deck = new String[]{"AS", "1S", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AD", "1D", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AC", "1C", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AH", "1H", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH"};
+
         // From the 52 card deck, select a random card
         int randomIndex = new Random().nextInt(52);
         String drawnCard = deck[randomIndex];
@@ -28,10 +36,16 @@ public class Deck {
         return cards;
     }
 
+    // Passes to the dealer's turn
+    public void stay() {
+
+    }
+
     // Total method tallies each card's value
-    public int total(String[] cards, int count, int total) {
-        // Initialize total to 0
+    public static int total(String[] cards, int count, int total) {
+        // Initialize variables
         total = 0;
+        int cardValue;
         // For each card until the total amount of cards has been reviewed,
         for (int i = 0; i <= count; i++) {
             // Select the current card
@@ -44,7 +58,8 @@ public class Deck {
             else if (card.contains("A")) {
                 if (total <= 10) {
                     cardValue = 11;
-                } else {
+                }
+                else {
                     cardValue = 1;
                 }
             }
--- PA1.java
+++ PA1.java
@@ -1,10 +1,37 @@
 //pa1
 import java.util.Scanner;  // Import the Scanner class
 
-public class PA1{
-    public static void main(String[] args){
+public class PA1 {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Let's Play BlackJack!\n");
+        System.out.println("A game of Blackjack:");
+
+        // Create starting hands
+        // PLAYER
+        String [] playerHand = new String[11];
+        int playerCount = 0;
+        Deck player = new Deck(playerHand, playerCount);
+
+        Deck.hit(playerHand, playerCount);
+        playerCount++;
+        Deck.hit(playerHand, playerCount);
+        playerCount++;
+
+
+        // DEALER
+        String [] dealerHand = new String[11];
+        int dealerCount = 0;
+        Deck dealer = new Deck(dealerHand, dealerCount);
+
+        Deck.hit(dealerHand, dealerCount);
+        dealerCount++;
+        dealerHand[1] = "XX";
+        dealerCount++;
+
+        // Initial display
+        Deck.display(dealerHand, playerHand);
+
+
 
     }
 }
