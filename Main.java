d7fe68c246c30dfdf2ab2be99ac6e1cbc7f06573
carahkatz <carah.katz@gmail.com>
2024-02-27 23:04:17 -0500

    Progress update.

--- Main.java
+++ Main.java
@@ -43,41 +43,43 @@ public class Main {
         Deck.display(dealerHand, playerHand, playerCount, playerValue);
 
         // Initial choice
-        int choice = input.nextInt();
-        while (choice != 1 && choice != 2) {
-            System.out.println("Please enter 1 or 2: ");
-            choice = input.nextInt();
-        }
-
-        // Loop through choices to hit
-        while (choice == 1) {
-            Deck.hit(playerHand, playerCount);
-            playerCount++;
-
-            playerValue = Deck.total(playerHand, playerCount, playerValue);
-            //System.out.println("Debug: " + playerValue);
-
-            if (playerValue < 21) {
-                Deck.display(dealerHand, playerHand, playerCount, playerValue);
-
-                // Error checked
+        if (playerValue != 21) {
+            // Get choice to hit or stay with error checking
+            int choice = input.nextInt();
+            while (choice != 1 && choice != 2) {
+                System.out.println("Please enter 1 or 2: ");
                 choice = input.nextInt();
-                while (choice != 1 && choice != 2) {
-                    System.out.println("Please enter 1 or 2: ");
+            }
+
+            // Loop through choices to hit
+            while (choice == 1) {
+                Deck.hit(playerHand, playerCount);
+                playerCount++;
+
+                playerValue = Deck.total(playerHand, playerCount, playerValue);
+                //System.out.println("Debug: " + playerValue);
+
+                if (playerValue < 21) {
+                    Deck.display(dealerHand, playerHand, playerCount, playerValue);
+
+                    // Get next choice with error checking
                     choice = input.nextInt();
+                    while (choice != 1 && choice != 2) {
+                        System.out.println("Please enter 1 or 2: ");
+                        choice = input.nextInt();
+                    }
+                } else if (playerValue == 21) {
+                    // Go to dealer's turn
+                } else {
+                    System.out.println("Dealer wins");
+                    break;
                 }
             }
-            else if (playerValue == 21) {
-                //Goes to dealer's turn
-            }
-            else {
-                System.out.println("Dealer wins");
-                break;
-            }
+
+            // Once stay is chosen, go to dealer's turn
+
+
         }
-
-        // Once stay is chosen, go to dealer's turn
-
-
+        // If initial playerValue IS 21, go to dealer's turn
     }
 }
